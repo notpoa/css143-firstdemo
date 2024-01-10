10206d4d71948317518c2a1df111c93af76c8ea7
vincentp <vincentp@outlook.com>
2024-01-10 11:15:59 -0800

    firsttime

---
+++ src/main/java/IsAPrimeNumber.java
@@ -0,0 +1,8 @@
+public class IsAPrimeNumber {
+    /*
+        This function returns true when the given number is a prime number
+     */
+    public static boolean isAPrimeNumber(long num) {  // DO NOT change the function name, return type, and parameter(s).
+        return false; // TODO: placeholder. To be replace with real code.
+    }
+}
